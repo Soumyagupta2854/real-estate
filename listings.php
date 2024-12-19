cbec4bab0af0b0265c4938c699300a793c2f00b8
Soumya Gupta <105983765+Soumyagupta2854@users.noreply.github.com>
2024-12-19 21:53:45 +0530

    Add files via upload

--- listings.php
+++ listings.php
@@ -1,14 +1,53 @@
-<?php  
+<?php
 
-include 'components/connect.php';
+include '../components/connect.php';
 
-if(isset($_COOKIE['user_id'])){
-   $user_id = $_COOKIE['user_id'];
+if(isset($_COOKIE['admin_id'])){
+   $admin_id = $_COOKIE['admin_id'];
 }else{
-   $user_id = '';
+   $admin_id = '';
+   header('location:login.php');
 }
 
-include 'components/save_send.php';
+if(isset($_POST['delete'])){
+
+   $delete_id = $_POST['delete_id'];
+   $delete_id = filter_var($delete_id, FILTER_SANITIZE_STRING);
+
+   $verify_delete = $conn->prepare("SELECT * FROM `property` WHERE id = ?");
+   $verify_delete->execute([$delete_id]);
+
+   if($verify_delete->rowCount() > 0){
+      $select_images = $conn->prepare("SELECT * FROM `property` WHERE id = ?");
+      $select_images->execute([$delete_id]);
+      while($fetch_images = $select_images->fetch(PDO::FETCH_ASSOC)){
+         $image_01 = $fetch_images['image_01'];
+         $image_02 = $fetch_images['image_02'];
+         $image_03 = $fetch_images['image_03'];
+         $image_04 = $fetch_images['image_04'];
+         $image_05 = $fetch_images['image_05'];
+         unlink('../uploaded_files/'.$image_01);
+         if(!empty($image_02)){
+            unlink('../uploaded_files/'.$image_02);
+         }
+         if(!empty($image_03)){
+            unlink('../uploaded_files/'.$image_03);
+         }
+         if(!empty($image_04)){
+            unlink('../uploaded_files/'.$image_04);
+         }
+         if(!empty($image_05)){
+            unlink('../uploaded_files/'.$image_05);
+         }
+      }
+      $delete_listings = $conn->prepare("DELETE FROM `property` WHERE id = ?");
+      $delete_listings->execute([$delete_id]);
+      $success_msg[] = 'Listing deleted!';
+   }else{
+      $warning_msg[] = 'Listing deleted already!';
+   }
+
+}
 
 ?>
 
@@ -18,121 +57,108 @@ include 'components/save_send.php';
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>All Listings</title>
+   <title>Listings</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="../css/admin_style.css">
 
 </head>
 <body>
    
-<?php include 'components/user_header.php'; ?>
-
-<!-- listings section starts  -->
+<!-- header section starts  -->
+<?php include '../components/admin_header.php'; ?>
+<!-- header section ends -->
 
 <section class="listings">
 
    <h1 class="heading">all listings</h1>
 
+   <form action="" method="POST" class="search-form">
+      <input type="text" name="search_box" placeholder="search listings..." maxlength="100" required>
+      <button type="submit" class="fas fa-search" name="search_btn"></button>
+   </form>
+
    <div class="box-container">
-      <?php
-         $total_images = 0;
-         $select_properties = $conn->prepare("SELECT * FROM `property` ORDER BY date DESC");
-         $select_properties->execute();
-         if($select_properties->rowCount() > 0){
-            while($fetch_property = $select_properties->fetch(PDO::FETCH_ASSOC)){
 
-            $select_user = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
-            $select_user->execute([$fetch_property['user_id']]);
-            $fetch_user = $select_user->fetch(PDO::FETCH_ASSOC);
-
-            if(!empty($fetch_property['image_02'])){
-               $image_coutn_02 = 1;
-            }else{
-               $image_coutn_02 = 0;
-            }
-            if(!empty($fetch_property['image_03'])){
-               $image_coutn_03 = 1;
-            }else{
-               $image_coutn_03 = 0;
-            }
-            if(!empty($fetch_property['image_04'])){
-               $image_coutn_04 = 1;
-            }else{
-               $image_coutn_04 = 0;
-            }
-            if(!empty($fetch_property['image_05'])){
-               $image_coutn_05 = 1;
-            }else{
-               $image_coutn_05 = 0;
-            }
-
-            $total_images = (1 + $image_coutn_02 + $image_coutn_03 + $image_coutn_04 + $image_coutn_05);
-
-            $select_saved = $conn->prepare("SELECT * FROM `saved` WHERE property_id = ? and user_id = ?");
-            $select_saved->execute([$fetch_property['id'], $user_id]);
-
-      ?>
-      <form action="" method="POST">
-         <div class="box">
-            <input type="hidden" name="property_id" value="<?= $fetch_property['id']; ?>">
-            <?php
-               if($select_saved->rowCount() > 0){
-            ?>
-            <button type="submit" name="save" class="save"><i class="fas fa-heart"></i><span>saved</span></button>
-            <?php
-               }else{ 
-            ?>
-            <button type="submit" name="save" class="save"><i class="far fa-heart"></i><span>save</span></button>
-            <?php
-               }
-            ?>
-            <div class="thumb">
-               <p class="total-images"><i class="far fa-image"></i><span><?= $total_images; ?></span></p>
-               
-               <img src="uploaded_files/<?= $fetch_property['image_01']; ?>" alt="">
-            </div>
-            <div class="admin">
-               <h3><?= substr($fetch_user['name'], 0, 1); ?></h3>
-               <div>
-                  <p><?= $fetch_user['name']; ?></p>
-                  <span><?= $fetch_property['date']; ?></span>
-               </div>
-            </div>
-         </div>
-         <div class="box">
-            <div class="price"><i class="fas fa-indian-rupee-sign"></i><span><?= $fetch_property['price']; ?></span></div>
-            <h3 class="name"><?= $fetch_property['property_name']; ?></h3>
-            <p class="location"><i class="fas fa-map-marker-alt"></i><span><?= $fetch_property['address']; ?></span></p>
-            <div class="flex">
-               <p><i class="fas fa-house"></i><span><?= $fetch_property['type']; ?></span></p>
-               <p><i class="fas fa-tag"></i><span><?= $fetch_property['offer']; ?></span></p>
-               <p><i class="fas fa-bed"></i><span><?= $fetch_property['bhk']; ?> BHK</span></p>
-               <p><i class="fas fa-trowel"></i><span><?= $fetch_property['status']; ?></span></p>
-               <p><i class="fas fa-couch"></i><span><?= $fetch_property['furnished']; ?></span></p>
-               <p><i class="fas fa-maximize"></i><span><?= $fetch_property['carpet']; ?>sqft</span></p>
-            </div>
-            <div class="flex-btn">
-               <a href="view_property.php?get_id=<?= $fetch_property['id']; ?>" class="btn">view property</a>
-               <input type="submit" value="send enquiry" name="send" class="btn">
-            </div>
-         </div>
-      </form>
-      <?php
-         }
+   <?php
+      if(isset($_POST['search_box']) OR isset($_POST['search_btn'])){
+         $search_box = $_POST['search_box'];
+         $search_box = filter_var($search_box, FILTER_SANITIZE_STRING);
+         $select_listings = $conn->prepare("SELECT * FROM `property` WHERE property_name LIKE '%{$search_box}%' OR address LIKE '%{$search_box}%' ORDER BY date DESC");
+         $select_listings->execute();
       }else{
-         echo '<p class="empty">no properties added yet! <a href="post_property.php" style="margin-top:1.5rem;" class="btn">add new</a></p>';
+         $select_listings = $conn->prepare("SELECT * FROM `property` ORDER BY date DESC");
+         $select_listings->execute();
+      } 
+      $total_images = 0;
+       if($select_listings->rowCount() > 0){
+         while($fetch_listing = $select_listings->fetch(PDO::FETCH_ASSOC)){
+
+         $listing_id = $fetch_listing['id'];
+
+         $select_user = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+         $select_user->execute([$fetch_listing['user_id']]);
+         $fetch_user = $select_user->fetch(PDO::FETCH_ASSOC);
+
+         if(!empty($fetch_listing['image_02'])){
+            $image_coutn_02 = 1;
+         }else{
+            $image_coutn_02 = 0;
+         }
+         if(!empty($fetch_listing['image_03'])){
+            $image_coutn_03 = 1;
+         }else{
+            $image_coutn_03 = 0;
+         }
+         if(!empty($fetch_listing['image_04'])){
+            $image_coutn_04 = 1;
+         }else{
+            $image_coutn_04 = 0;
+         }
+         if(!empty($fetch_listing['image_05'])){
+            $image_coutn_05 = 1;
+         }else{
+            $image_coutn_05 = 0;
+         }
+
+         $total_images = (1 + $image_coutn_02 + $image_coutn_03 + $image_coutn_04 + $image_coutn_05);
+   ?>
+   <div class="box">
+      <div class="thumb">
+         <p><i class="far fa-image"></i><span><?= $total_images; ?></span></p>
+         <img src="../uploaded_files/<?= $fetch_listing['image_01']; ?>" alt="">
+      </div>
+      <p class="price"><i class="fas fa-indian-rupee-sign"></i><?= $fetch_listing['price']; ?></p>
+      <h3 class="name"><?= $fetch_listing['property_name']; ?></h3>
+      <p class="location"><i class="fas fa-map-marker-alt"></i><?= $fetch_listing['address']; ?></p>
+      <form action="" method="POST">
+         <input type="hidden" name="delete_id" value="<?= $listing_id; ?>">
+         <a href="view_property.php?get_id=<?= $listing_id; ?>" class="btn">view property</a>
+         <input type="submit" value="delete listing" onclick="return confirm('delete this listing?');" name="delete" class="delete-btn">
+      </form>
+   </div>
+   <?php
+         }
+      }elseif(isset($_POST['search_box']) OR isset($_POST['search_btn'])){
+         echo '<p class="empty">no results found!</p>';
+      }else{
+         echo '<p class="empty">no property posted yet!</p>';
       }
-      ?>
-      
+   ?>
+
    </div>
 
 </section>
 
-<!-- listings section ends -->
+
+
+
+
+
+
 
 
 
@@ -147,12 +173,10 @@ include 'components/save_send.php';
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
-<?php include 'components/footer.php'; ?>
-
 <!-- custom js file link  -->
-<script src="js/script.js"></script>
+<script src="../js/admin_script.js"></script>
 
-<?php include 'components/message.php'; ?>
+<?php include '../components/message.php'; ?>
 
 </body>
 </html>
